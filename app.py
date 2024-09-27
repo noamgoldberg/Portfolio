c6757e9e90f46413498b95154c0612fafa4ac229
Noam Goldberg <noamgoldberg2@gmail.com>
2024-09-27 16:44:24 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def main():
+    st.title("Noam Goldberg's Portfolio")
+    with st.container(border=True):
+        st.header("App 1")
+
+if __name__ == "__main__":
+    main()
